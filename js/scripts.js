ba79ae544f071a0cbbc7bf2def0e95692b6115bc
daniel970-max <danielgln1999@gmail.com>
2024-04-03 18:07:42 -0500

    Mejorardo y dando final al proyecto

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,97 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Navbar shrink function
-  var navbarShrink = function () {
-    const navbarCollapsible = document.body.querySelector("#mainNav");
-    if (!navbarCollapsible) {
-      return;
-    }
-    if (window.scrollY === 0) {
-      navbarCollapsible.classList.remove("navbar-shrink");
-    } else {
-      navbarCollapsible.classList.add("navbar-shrink");
-    }
-  };
+window.addEventListener("DOMContentLoaded", () => {
+  switch (navigator.language) {
+    case "en-US":
+      establecerIdioma("English");
+      changeLanguage("en");
+      break;
+    case "es-ES":
+      establecerIdioma("Español");
+      changeLanguage("es");
+      break;
 
-  // Shrink the navbar
-  navbarShrink();
-
-  // Shrink the navbar when page is scrolled
-  document.addEventListener("scroll", navbarShrink);
-
-  //  Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector("#mainNav");
-  if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: "#mainNav",
-      rootMargin: "0px 0px -40%",
-    });
+    default:
+      break;
   }
+});
 
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = [].slice.call(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
-      }
-    });
+// Navbar shrink function
+var navbarShrink = function () {
+  const navbarCollapsible = document.body.querySelector("#mainNav");
+  if (!navbarCollapsible) {
+    return;
+  }
+  if (window.scrollY === 0) {
+    navbarCollapsible.classList.remove("navbar-shrink");
+  } else {
+    navbarCollapsible.classList.add("navbar-shrink");
+  }
+};
+
+// Shrink the navbar
+navbarShrink();
+
+// Shrink the navbar when page is scrolled
+document.addEventListener("scroll", navbarShrink);
+
+//  Activate Bootstrap scrollspy on the main nav element
+const mainNav = document.body.querySelector("#mainNav");
+if (mainNav) {
+  new bootstrap.ScrollSpy(document.body, {
+    target: "#mainNav",
+    rootMargin: "0px 0px -40%",
+  });
+}
+
+// Collapse responsive navbar when toggler is visible
+const navbarToggler = document.body.querySelector(".navbar-toggler");
+const responsiveNavItems = [].slice.call(
+  document.querySelectorAll("#navbarResponsive .nav-link")
+);
+responsiveNavItems.map(function (responsiveNavItem) {
+  responsiveNavItem.addEventListener("click", () => {
+    if (window.getComputedStyle(navbarToggler).display !== "none") {
+      navbarToggler.click();
+    }
   });
 });
+//translate
+const idiomaActual = document.getElementById("idioma");
+const listaIdiomas = document.getElementById("idiomas");
+const idiomas = document.getElementsByClassName("option");
+idiomaActual.addEventListener("click", (e) => {
+  e.preventDefault();
+  listaIdiomas.classList.toggle("idiomasActive");
+});
+//buscar las secciones
+const textsToChange = document.querySelectorAll("[data-section]");
+//llamado de json language
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`languages/${language}.json`);
+  const texts = await requestJson.json();
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+    textToChange.innerHTML = texts[section][value];
+  }
+};
+//click en el lenguage
+const opcionesArray = Array.from(idiomas);
+opcionesArray.forEach((opcion) => {
+  opcion.addEventListener("click", (e) => {
+    e.preventDefault();
+    changeLanguage(e.target.parentElement.dataset.language);
+    listaIdiomas.classList.toggle("idiomasActive");
+    const idioma = opcion
+      .getElementsByTagName("span")[0]
+      .textContent.toLocaleLowerCase();
+    establecerIdioma(idioma);
+  });
+});
+function establecerIdioma(idioma) {
+  idiomaActual.getElementsByTagName(
+    "img"
+  )[0].src = `assets/banderas/${idioma}.jpg`;
+}
 
 window.addEventListener("load", () => {
   new Glider(document.querySelector(".carusel_lista"), {
@@ -96,4 +148,20 @@ function aos_init() {
 }
 window.addEventListener("load", () => {
   aos_init();
+});
+
+//formulario
+const formContact = document.getElementById("contactForm");
+
+formContact.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+  const send = `Reciba un cordial saludo de ${name} con nùmero de telèfono ${phone} y email ${email} con mensaje : ${message}`;
+  var messageSend = encodeURIComponent(send);
+  var number = encodeURIComponent("+593988497045");
+  window.location.href = "https://wa.me/" + number + "?text=" + messageSend;
+  formContact.reset();
 });
